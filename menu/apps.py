e698eb97f7499f03436d6164e2ea641ca6295eb0
Pietro Giuliano <cinelligrafica@gmail.com>
2025-03-04 16:38:27 +0100

    Aggiunto comando per creare superuser automaticamente

--- menu/apps.py
+++ menu/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class MenuConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'menu'
+
+    def ready(self):
+        from django.core.management import call_command
+        call_command("create_superuser")
